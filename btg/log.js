6ede8045cba0e29d1c37e16e5da62c2e5627bd1f
webhost5 <webtest532@proton.me>
2025-03-23 17:26:08 +0000

    add head using js

--- btg/log.js
+++ btg/log.js
@@ -19,6 +19,20 @@ if (hashedInput === expectedHash) {
     document.body.appendChild(scriptElement);
 
 
+    document.head.innerHTML += `
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.2.0/crypto-js.min.js"></script>
+    <link rel="stylesheet" href="./style.css">
+`;
+
+    document.title = "Google";
+
+
+
 } else {
     document.body.innerHTML = "<h1>Access Denied</h1>"; // Optionally replace body content with a message
 }
+
+
